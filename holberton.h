cd219558c5f7178b99c7686a5277cf8b3a0077a4
apla02 <1873@holbertonschool.com>
2020-07-20 13:09:52 -0500

    prototipe of %d and %i

--- holberton.h
+++ holberton.h
@@ -1,5 +1,10 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
+#include <stdio.h> /* moved from _printf.c */
+#include <unistd.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <stdlib.h>
 typedef struct id_function
 {
 	char id;
@@ -9,5 +14,5 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int _printf_c(va_list flist);
 int _printf_s(va_list flist);
-int _printf_i(va_list flist);
+int _printf_number(va_list flist);
 #endif
