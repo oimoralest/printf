26d1b47540719c0a7005de2854f5a518e47473da
apla02 <1873@holbertonschool.com>
2020-07-21 09:33:52 -0500

    new function for convert decimal to binary

--- _print_binary.c
+++ _print_binary.c
@@ -1,27 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main()
+#include "holberton.h"
+/**
+ * _print_b - convert a decimal in a base 2
+ *  @flist: list of arguments int
+ *  Return: len
+ */
+int _print_b(va_list flist)
 {
-    int n = 200;
-    int i = 0;
-    int j = 0;
-    char s[10];
+	int base = 2, c = 1, len = 0, n = va_arg(flist, int);
+	char w = 0;
 
-    if (n == 0)
-    {
-    putchar(n);
-    }
-    while (n > 0)
-    {
-    s[i] = n % 2;
-    n = n / 2;
-    ++i;
-    }
-
-    for (j = i - 1; j >= 0; --j)
-    {
-    printf("%d", s[j]);
-    }
-    putchar('\n');
-    return 0;
+	if (n <= 0)
+	{
+		putchar('0');
+	}
+	else
+	{
+		while (c > 0)
+		{
+			if ((n / base) >= 2)
+			{
+				base *= 2;
+				c++;
+			}
+			else
+			{
+				w = (n / base) + '0';
+				write(1, &w, 1);
+				n %= base;
+				base /= 2;
+				len++;
+				c--;
+			}
+		}
+		w = n + '0';
+		write(1, &w, 1);
+	}
+	return (++len);
 }
