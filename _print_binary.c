ea0b84e5b920bb99044cf8c0b65c11aa5014cfad
apla02 <1873@holbertonschool.com>
2020-07-21 11:45:55 -0500

    removed long

--- _print_binary.c
+++ _print_binary.c
@@ -6,8 +6,8 @@
  */
 int _print_b(va_list flist)
 {
-	unsigned long int base = 2;
-	unsigned long int n = va_arg(flist, unsigned long int);
+	unsigned int base = 2;
+	unsigned int n = va_arg(flist, unsigned int);
 	int c = 1, len = 0;
 	char w = '0';
 
